ab5a13c52606ae9d8157cf673596a70149d72db1
jyothsna102515 <76741288+jyothsna102515@users.noreply.github.com>
2021-01-21 09:05:19 +0530

    popups

--- byndr-ui/src/components/common/Cards/grpHeadCard.js
+++ byndr-ui/src/components/common/Cards/grpHeadCard.js
@@ -107,7 +107,7 @@ export default function GrpHeadCard(props) {
                         </Grid>
                     </Grid>
                 </ListItemText>
-                <ListItemSecondaryAction style={{ float: "right", position: "relative", marginTop: 17, width: "25%" }}>
+                <ListItemSecondaryAction style={{ float: "right", position: "relative", marginTop: 17, width: "185px" }}>
                     <Grid item xs={12}>
                         <Grid container spacing={2}>
                             <Typography variant="body2" component="span" className={classes.lastUpdate}>
--- byndr-ui/src/components/common/popups/GroupMoreInfo.js
+++ byndr-ui/src/components/common/popups/GroupMoreInfo.js
@@ -2,25 +2,46 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import List from '@material-ui/core/List';
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
-import ReportIcon from '@material-ui/icons/Report';
-import BlockIcon from '@material-ui/icons/Block';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import VolumeMuteIcon from '@material-ui/icons/VolumeMute';
+import PinDropIcon from '@material-ui/icons/PinDrop';
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import PersonAddDisabledIcon from '@material-ui/icons/PersonAddDisabled';
+import CameraAltIcon from '@material-ui/icons/CameraAlt';
 
 const GroupMoreInfoPopup = () => {
     return (<List component="nav"  >
         <ListItem button>
             <ListItemIcon>
-                <ReportIcon />
+                <PinDropIcon />
             </ListItemIcon>
-            <ListItemText primary="Report" />
+            <ListItemText primary="Pin to Top" />
         </ListItem>
         <ListItem button>
             <ListItemIcon>
-                <BlockIcon />
+                <AccountCircleOutlinedIcon />
             </ListItemIcon>
-            <ListItemText primary="Block" />
+            <ListItemText primary="Rename" />
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <ExitToAppOutlinedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Leave" />
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <PersonAddIcon />
+            </ListItemIcon>
+            <ListItemText primary="Add Participant" />
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <PersonAddDisabledIcon />
+            </ListItemIcon>
+            <ListItemText primary="Remove Participant" />
         </ListItem>
         <ListItem button>
             <ListItemIcon>
@@ -30,9 +51,9 @@ const GroupMoreInfoPopup = () => {
         </ListItem>
         <ListItem button>
             <ListItemIcon>
-                <DeleteForeverIcon />
+                <CameraAltIcon />
             </ListItemIcon>
-            <ListItemText primary="Delete Chat" />
+            <ListItemText primary="Change Image" />
         </ListItem>
     </List>);
 }
